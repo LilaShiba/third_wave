6a708a9d0e84d3302fc68df4c3afa10eba2ba28e
kyle1james <lilaresearch@gmail.com>
2024-03-04 12:11:42 -0500

    record

--- app/recorded_data/tricorder.py
+++ app/recorded_data/tricorder.py
@@ -15,13 +15,14 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor_tsl2591 = adafruit_tsl2591.TSL2591(i2c)
 sensor_lsm9ds1 = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
+
 def record_sensor_data(csv_file_path, duration_seconds):
     """Records real sensor data to a CSV file."""
     end_time = datetime.now() + timedelta(seconds=duration_seconds)
     with open(csv_file_path, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Timestamp', 'Accel_X', 'Accel_Y', 'Accel_Z', 
-                         'Gyro_X', 'Gyro_Y', 'Gyro_Z', 'Mag_X', 'Mag_Y', 'Mag_Z', 
+        writer.writerow(['Timestamp', 'Accel_X', 'Accel_Y', 'Accel_Z',
+                         'Gyro_X', 'Gyro_Y', 'Gyro_Z', 'Mag_X', 'Mag_Y', 'Mag_Z',
                          'Lux', 'IR', 'Full_Spectrum'])
 
         while datetime.now() < end_time:
@@ -36,14 +37,16 @@ def record_sensor_data(csv_file_path, duration_seconds):
             full_spectrum = sensor_tsl2591.visible + infrared
 
             timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-            writer.writerow([timestamp, accel_x, accel_y, accel_z, 
-                             gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z, 
+            writer.writerow([timestamp, accel_x, accel_y, accel_z,
+                             gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z,
                              lux, infrared, full_spectrum])
 
             time.sleep(1)
 
+
 if __name__ == "__main__":
-    csv_file_path = './sensor_data_real.csv'
+    csv_file_path = './sensor_data_' + \
+        datetime.now().strftime('%Y-%m-%dT%H:%M') + '.csv'
     duration_seconds = 120  # For example, record data for 2 minutes
     record_sensor_data(csv_file_path, duration_seconds)
     print(f'Data recorded to {csv_file_path}')
--- hardware.py
+++
@@ -1,9 +0,0 @@
-from adafruit_lsm9ds1 import LSM9DS1_I2C
-from adafruit_tsl2591 import TSL2591
-import board
-import busio
-
-i2c = busio.I2C(board.SCL, board.SDA)
-# Initialize sensors
-movement_sensors = LSM9DS1_I2C(i2c)
-#light_sensor = TSL2591(i2c)
